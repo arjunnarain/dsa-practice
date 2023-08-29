96133afd4725146eb9ce630cbb16130dc2422ba0
Arjun Narain <54548286+arjunnarain@users.noreply.github.com>
2023-08-30 00:06:05 +0530

    Leetcode - Practice 29/08/2023 (#8)

---
+++ neetcode/dsa_course/binary_search/278_first_bad_version.py
@@ -0,0 +1,21 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low, high, result = 1, n, 1
+        while low <= high:
+            mid = (low + high) // 2
+            if not isBadVersion(mid):
+               low = mid + 1
+            else:
+                high = mid - 1
+                result = mid
+                
+        return result 
