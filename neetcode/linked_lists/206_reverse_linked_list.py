58c79817c6f4c78c189163aa174550e30069ac4c
arjunnarain <narain.arjun@gmail.com>
2023-08-31 13:02:14 +0530

    Removes redundant structure

--- neetcode/linked_lists/206_reverse_linked_list.py
+++
@@ -1,9 +0,0 @@
-# recursive solution
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
-        newHead = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return newHead
