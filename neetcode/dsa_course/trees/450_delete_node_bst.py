059b9e6e843a7f7b05190bb900d5fe1bfe5bf411
arjunnarain <narain.arjun@gmail.com>
2023-09-11 16:10:29 +0530

    Solves leetcode 450

---
+++ neetcode/dsa_course/trees/450_delete_node_bst.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findMinNode(self, curr):
+        while curr.left:
+            curr = curr.left
+        return curr
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return root
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            curr = self.findMinNode(root.right)
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, curr.val)
+        return root
+        
